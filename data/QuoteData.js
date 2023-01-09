05d4337722f7883e77bc266d25ad6e5b545b81dc
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-08 18:20:48 -0700

    added QuoteData file to add to QUOTES object using Quote Constructor

---
+++ data/QuoteData.js
@@ -0,0 +1,102 @@
+import Quote from "./QuoteConstructor";
+
+export const QUOTES = [
+	new Quote(
+		"Unknown",
+		"A bend in the road is not the end of the road... unless you fail to make the turn."
+	),
+	new Quote(
+		"Anonymous",
+		"A friend by your side can keep you warmer than the most expensive coat."
+	),
+	new Quote(
+		"Hugh Downs",
+		"A happy person is not a person in a certain set of circumstances, but rather a person with a certain set of attitudes."
+	),
+	new Quote(
+		"Anonymous",
+		"A healthy attitude is contagious, but don't wait to catch it from others. Be a carrier."
+	),
+	new Quote(
+		"Christopher Reeve",
+		"A hero is an ordinary individual who finds the strength to persevere and endure in spite of overwhelming obstacles."
+	),
+	new Quote(
+		"Jean Kerr",
+		"A lawyer is never entirely comfortable with a friendly divorce, anymore than a good mortician wants to finish his job and then have the patient sit up on the table."
+	),
+	new Quote(
+		"Tenneva Jordan",
+		"A mother is a person who seeing there are only four pieces of pie for five people, promptly announces she never did care for pie."
+	),
+	new Quote("Jewish Proverb", "A mother understands what a child does not say."),
+	new Quote(
+		"Richard L. Evans",
+		"A person soon learns how little he knows when a child begins to ask questions."
+	),
+	new Quote("Mohammed", "A person's true wealth is the good he or she does in the world."),
+	new Quote(
+		"Unknown",
+		"A positive attitude will not solve all the problems in the world, but it will annoy enough people to make it worth the effort."
+	),
+	new Quote("Unknown", "A smile confuses an approaching frown."),
+	new Quote("Phyllis Diller", "A smile is a curve that sets everything straight. "),
+	new Quote(
+		"David Brinkley",
+		"A successful man is one who can lay a firm foundation with the bricks others have thrown at him."
+	),
+	new Quote(
+		"Len Wein",
+		"A true friend is someone who is there for you when he'd rather be anywhere else."
+	),
+	new Quote(
+		"Unknown",
+		"A truly rich man is one whose children run into his arms when his hands are empty."
+	),
+	new Quote("Leonardo Da Vinci", "A well-spent day brings happy sleep."),
+	new Quote(
+		"Mae West",
+		"A woman has got to love a bad man once or twice in her life to be thankful for a good one."
+	),
+	new Quote("Karen Lamb", "A year from now you will wish you had started today."),
+	new Quote(
+		"Lou Holtz",
+		"Ability is what you're capable of doing. Motivation determines what you do. Attitude determines how well you do it."
+	),
+	new Quote(
+		"John Wooden",
+		"Ability may get you to the top, but it takes character to keep you there."
+	),
+	new Quote(
+		"Les Brown",
+		"Accept responsibility for your life. Know that it is you who will get you where you want to go, no one else."
+	),
+	new Quote(
+		"Lou Holtz",
+		"Adversity is another way to measure the greatness of individuals. I never had a crisis that didn't make me stronger."
+	),
+	new Quote(
+		"Erica Jong",
+		"Advice is what we ask for when we already know the answer, but wish we didn't."
+	),
+	new Quote(
+		"Joan Collins",
+		"Age is just a number. It's totally irrelevant unless, of course, you happen to be a bottle of wine."
+	),
+	new Quote("Anonymous", "Aim high. Chances are, you won't be disappointed."),
+	new Quote(
+		"Judy Garland",
+		"Always be a first-rate version of yourself, instead of a second-rate version of somebody else."
+	),
+	new Quote(
+		"Unkown",
+		"Always remember that in some way or another you’re somebody’s angel, even if you don't know it…"
+	),
+	new Quote(
+		"Ann Landers",
+		"An open marriage is nature's way of telling you that you need a divorce."
+	),
+	// new Quote("", ""),
+	// new Quote("", ""),
+	// new Quote("", ""),
+];
