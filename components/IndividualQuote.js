f70da8624a61466c611e575b58e90c52e54f56d1
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-08 18:38:41 -0700

    imported IndividualQuote component to App.js and set up randomNumber, authorOfTheDay, quoteOfTheDay with useState and set those values inside of useLayoutEffect to run at beginning of program

--- components/IndividualQuote.js
+++ components/IndividualQuote.js
@@ -1,10 +1,22 @@
+import { useLayoutEffect, useState } from "react";
 import { View, Text } from "react-native";
 import { QUOTES } from "../data/QuoteData";
 
 function IndividualQuote() {
+	const [randomNumber, setRandomNumber] = useState(0);
+	const [authorOfTheDay, setAuthorOfTheDay] = useState();
+	const [quoteOfTheDay, setQuoteOfTheDay] = useState();
+
+	useLayoutEffect(() => {
+		setRandomNumber(Math.random() * 10);
+		setQuoteOfTheDay(QUOTES[randomNumber].actualQuote);
+		setAuthorOfTheDay(QUOTES[randomNumber].author);
+	}, []);
+
 	return (
 		<View>
-			<Text>Welcome to your quote for the day!</Text>
+			<Text>{authorOfTheDay}</Text>
+			<Text>{quoteOfTheDay}</Text>
 		</View>
 	);
 }
