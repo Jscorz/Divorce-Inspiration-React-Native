0680760cdb18b1617c5dce27bd5cd0255902a299
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-08 18:25:29 -0700

    built placeholder component for IndividualQuote and imported QUOTES object from data

---
+++ components/IndividualQuote.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import { QUOTES } from "../data/QuoteData";
+
+function IndividualQuote() {
+	return (
+		<View>
+			<Text>Welcome to your quote for the day!</Text>
+		</View>
+	);
+}
+
+export default IndividualQuote;
