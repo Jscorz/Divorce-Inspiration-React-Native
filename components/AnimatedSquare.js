24171088a4c61464dbf5af7bd4f1ca6f90e20c8c
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-14 18:22:37 -0700

    updated color of animatedSquare back to secondaryLowOpacity

--- components/AnimatedSquare.js
+++ components/AnimatedSquare.js
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
 	square: {
 		width: 100,
 		height: 100,
-		backgroundColor: Colors.secondary500,
+		backgroundColor: Colors.secondaryLowOpacity,
 		marginTop: 120,
 	},
 });
