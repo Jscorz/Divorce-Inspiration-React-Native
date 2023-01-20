fc127c86ddb609826f7599854e7f4f12ff3c7c50
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-19 18:50:37 -0700

    assembled some of the article data into the correct format using Article class constructor

---
+++ data/ArticleData.js
@@ -0,0 +1,70 @@
+import Article from "./ArticleConstructor";
+
+export const ARTICLES = [
+	new Article(
+		"https://www.womansdivorce.com/50-50-custody.html",
+		"50-50 Custody",
+		"12/09/09",
+		"50/50 Custody: Smart Solution or Modern Mess"
+	),
+	new Article(
+		"https://www.womansdivorce.com/abusive-husband.html",
+		"Abusive Husband",
+		"10/26/2009",
+		"Divorcing an Abusive Husband "
+	),
+	new Article(
+		"https://www.womansdivorce.com/accepting-divorce.html",
+		"Accepting Divorce",
+		"3/15/2009",
+		"Accepting Divorce and Moving On - Acknowledging Reality and Being Grateful Anyway"
+	),
+	new Article(
+		"https://www.womansdivorce.com/afford-a-divorce.html",
+		"Afford A Divorce",
+		"10/10/2006",
+		"There are ways to get a divorce, even if your funds are limited."
+	),
+	new Article(
+		"https://www.womansdivorce.com/afford-divorce.html",
+		"Afford Divorce",
+		"11/27/2009",
+		"Can You Afford Divorce in Hard Times?"
+	),
+	new Article(
+		"https://www.womansdivorce.com/after-a-failed-marriage.html",
+		"After a Failed Marriage",
+		"10/24/2019",
+		"Coming to terms with a Failed Marriage"
+	),
+	new Article(
+		"https://www.womansdivorce.com/alienating-parent.html",
+		"Alienating Parent",
+		"7/25/2005",
+		"Are You An Alienating Parent?"
+	),
+	new Article(
+		"https://www.womansdivorce.com/alimony.html",
+		"Alimony",
+		"8/21/2006",
+		"Understanding Alimony - How Much to Expect or If You'll Have to Pay"
+	),
+	new Article(
+		"https://www.womansdivorce.com/being-strong.html",
+		"Being Strong",
+		"9/1/2009",
+		"Adopting the Right Mindset for Your Kid’s Sake"
+	),
+	new Article(
+		"https://www.womansdivorce.com/better-divorce.html",
+		"Better Divorce",
+		"2/13/2015",
+		"Tips for a Better Divorce"
+	),
+	new Article(
+		"https://www.womansdivorce.com/budgeting-income.html",
+		"Budgeting Income",
+		"6/9/2008",
+		"Making Ends Meet after Divorce"
+	),
+];
