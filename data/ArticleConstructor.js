32f9e48a5f3a976a4a10ded6b0b8a4adda1ed83c
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-19 18:30:26 -0700

    added Article class with constructor to add Articles/links later on

---
+++ data/ArticleConstructor.js
@@ -0,0 +1,9 @@
+class Article {
+	constructor(url, page, dateBuilt, linkText) {
+		this.url = url;
+		this.page = page;
+		this.dateBuilt = dateBuilt;
+		this.linkText = linkText;
+	}
+}
+export default Article;
