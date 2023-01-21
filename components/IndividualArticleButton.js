74f1f82941d5d49dab51ec6ec726effa5339cd33
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-21 15:17:23 -0700

    added imports for elements from react native and useEffect and useState from react which I used to set the suggestedArticle state value, and lastly a consoleLogger function to check data

--- components/IndividualArticleButton.js
+++ components/IndividualArticleButton.js
@@ -1,15 +1,31 @@
-import { Pressable } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Pressable, Text, StyleSheet } from "react-native";
 import { ARTICLES } from "../data/ArticleData";
 import Colors from "../constants/colors";
 
 function IndividualArticleButton() {
+	const randomNumber = Math.floor(Math.random() * (5 - 1) + 1);
+	const [suggestedArticle, setSuggestedArticle] = useState();
+
+	useEffect(() => {
+		setSuggestedArticle(ARTICLES[randomNumber]);
+	}, []);
+
+	function consoleLogger() {
+		console.log(suggestedArticle);
+	}
+
 	return (
 		<View>
 			<Pressable>
-				<Text>View an article that may help you to unlock more quotes</Text>
+				<Text onPress={consoleLogger}>
+					View an article that may help you to unlock more quotes
+				</Text>
 			</Pressable>
 		</View>
 	);
 }
 
 export default IndividualArticleButton;
+
+const styles = StyleSheet.create({});
