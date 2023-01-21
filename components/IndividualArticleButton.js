5f95ea48b1807097a1d80ccac95ea0149bf857fc
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-21 16:14:39 -0700

    removed import for useCallback

--- components/IndividualArticleButton.js
+++ components/IndividualArticleButton.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useCallback } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Pressable, Text, StyleSheet, Animated } from "react-native";
 import { ARTICLES } from "../data/ArticleData";
 import Colors from "../constants/colors";
