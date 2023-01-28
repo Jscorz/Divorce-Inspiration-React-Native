13f523c7e3327b9dec78ad00f64714eb18f1339e
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-28 11:25:16 -0700

    added images folder within assets folder to store images, added background image generated from midjourney and added it to app.js as background with opacity 0.15 to show backgroundImage and linear gradient both

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 import { useState, useEffect } from "react";
-import { StyleSheet, View, SafeAreaView } from "react-native";
+import { StyleSheet, View, SafeAreaView, ImageBackground } from "react-native";
 import Colors from "./constants/colors";
 import QuoteDisplayScreen from "./screens/QuoteDisplayScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
@@ -22,19 +23,34 @@ export default function App() {
 	}, []);
 
 	return (
-		<SafeAreaView style={styles.container}>
-			<View>{screen}</View>
-			{/* <WelcomeScreen /> */}
-			<StatusBar style='auto' />
-		</SafeAreaView>
+		<LinearGradient colors={[Colors.primary700, Colors.accent600]} style={styles.rootScreen}>
+			<ImageBackground
+				source={require("./assets/images/modern-background.png")}
+				resizeMode='cover'
+				style={styles.rootScreen}
+				imageStyle={styles.backgroundImage}
+			>
+				<SafeAreaView style={styles.container}>
+					<View>{screen}</View>
+					{/* <WelcomeScreen /> */}
+					<StatusBar style='auto' />
+				</SafeAreaView>
+			</ImageBackground>
+		</LinearGradient>
 	);
 }
 
 const styles = StyleSheet.create({
+	rootScreen: {
+		flex: 1,
+	},
 	container: {
 		flex: 1,
 		alignItems: "center",
 		justifyContent: "center",
-		backgroundColor: Colors.primary500,
+		// backgroundColor: Colors.primary500,
+	},
+	backgroundImage: {
+		opacity: 0.15,
 	},
 });
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -22,7 +22,7 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center",
-		backgroundColor: Colors.primary500,
+		// backgroundColor: Colors.primary500,
 		margin: 4,
 	},
 	welcomeText: {
