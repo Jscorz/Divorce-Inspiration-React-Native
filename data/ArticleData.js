38707b33f4d64a835424a9c72125766f57af9a80
Justin Scorzafava <jkscorzafava@gmail.com>
2023-01-29 11:15:10 -0700

    assembled more article data into the correct format, removed date built part of data

--- data/ArticleData.js
+++ data/ArticleData.js
@@ -4,193 +4,222 @@ export const ARTICLES = [
 	new Article(
 		"https://www.womansdivorce.com/50-50-custody.html",
 		"50-50 Custody",
-		"12/09/09",
 		"50/50 Custody: Smart Solution or Modern Mess"
 	),
 	new Article(
 		"https://www.womansdivorce.com/abusive-husband.html",
 		"Abusive Husband",
-		"10/26/2009",
 		"Divorcing an Abusive Husband "
 	),
 	new Article(
 		"https://www.womansdivorce.com/abusive-relationship.html",
 		"Abusive Relationship",
-		"4/6/2009",
 		"Separating From and Divorcing an Abusive Partner"
 	),
 	new Article(
 		"https://www.womansdivorce.com/accepting-divorce.html",
 		"Accepting Divorce",
-		"3/15/2009",
 		"Accepting Divorce and Moving On - Acknowledging Reality and Being Grateful Anyway"
 	),
 	new Article(
 		"https://www.womansdivorce.com/adopted-parents.html",
 		"Adopted Parents",
-		"5/31/2008",
 		"Legal Rights of Adopted Parents - How Adoption Impacts Custody"
 	),
 	new Article(
 		"https://www.womansdivorce.com/adult-children-after-divorce.html",
 		"Adult Children After Divorce",
-		"5/25/2006",
 		"Dealing With Adult Children After Divorce"
 	),
 	new Article(
 		"https://www.womansdivorce.com/adult-children-divorce.html",
 		"Adult Children Divorce",
-		"5/18/2006",
 		"How to Help When Your Adult Children Divorce"
 	),
 	new Article(
 		"https://www.womansdivorce.com/afford-a-divorce.html",
 		"Afford A Divorce",
-		"10/10/2006",
 		"There are ways to get a divorce, even if your funds are limited."
 	),
 	new Article(
 		"https://www.womansdivorce.com/afford-divorce.html",
 		"Afford Divorce",
-		"11/27/2009",
 		"Can You Afford Divorce in Hard Times?"
 	),
 	new Article(
 		"https://www.womansdivorce.com/after-a-failed-marriage.html",
 		"After a Failed Marriage",
-		"10/24/2019",
 		"Coming to terms with a Failed Marriage"
 	),
 	new Article(
 		"https://www.womansdivorce.com/after-the-divorce.html",
 		"After The Divorce",
-		"2003",
 		"The Importance of Taking Care of Yourself after Divorce"
 	),
 	new Article(
 		"https://www.womansdivorce.com/alienated-parent.html",
 		"Alienated Parent",
-		"6/15/2009",
 		"Learning to Live with Parental Alienation"
 	),
 	new Article(
 		"https://www.womansdivorce.com/alienating-parent.html",
 		"Alienating Parent",
-		"7/25/2005",
 		"Are You An Alienating Parent?"
 	),
 	new Article(
 		"https://www.womansdivorce.com/alimony.html",
 		"Alimony",
-		"8/21/2006",
 		"Understanding Alimony - How Much to Expect or If You'll Have to Pay"
 	),
 	new Article(
 		"https://www.womansdivorce.com/anger.html",
 		"Anger",
-		"10/31/2001",
 		"Tips for Releasing with the Lingering Divorce Anger"
 	),
 	new Article(
 		"https://www.womansdivorce.com/annulment-vs-divorce.html",
 		"Annulment vs Divorce",
-		"7/31/18",
 		"Is Annulment or Divorce the Best Solution to End Your Marriage"
 	),
 	new Article(
 		"https://www.womansdivorce.com/attorney-meeting-your-expectations.html",
 		"Attorney Meeting Your Expecations",
-		"10/30/2012",
 		"How to Assess if a Divorce Attorney will Meet Your Expectations"
 	),
 	new Article(
 		"https://www.womansdivorce.com/baby-save-a-marriage.html",
 		"Baby Save a Marriage",
-		"7/16/2016",
 		"Can Having a Baby Save Your Struggling Marriage?"
 	),
 	new Article(
 		"https://www.womansdivorce.com/being-strong.html",
 		"Being Strong",
-		"9/1/2009",
 		"Adopting the Right Mindset for Your Kid’s Sake"
 	),
 	new Article(
 		"https://www.womansdivorce.com/better-divorce.html",
 		"Better Divorce",
-		"2/13/2015",
 		"Tips for a Better Divorce"
 	),
 	new Article(
 		"https://www.womansdivorce.com/bifurcated-divorce.html",
 		"Bifurcated Divorce",
-		"8/4/2009",
 		"Why Would Someone Want a Bifurcated Divorce?"
 	),
 	new Article(
 		"https://www.womansdivorce.com/birthdays-after-divorce.html",
 		"Birthday After Divorce",
-		"7/4/2008",
 		"Birthdays After Divorce and My Perfect (ex) Husband"
 	),
 	new Article(
 		"https://www.womansdivorce.com/blended-family.html",
 		"Blended Family",
-		"2/7/2005",
 		"Blended Family Problems - Counseling just for the Child is Not Enough"
 	),
 	new Article(
 		"https://www.womansdivorce.com/blending-families.html",
 		"Blending Families",
-		"8/28/2010",
 		"Blending Families - Tips to Help Everyone Adjust when Expecting a New Baby"
 	),
 	new Article(
 		"https://www.womansdivorce.com/budgeting-income.html",
 		"Budgeting Income",
-		"6/9/2008",
 		"Making Ends Meet after Divorce"
 	),
 	new Article(
 		"https://www.womansdivorce.com/buying-a-house-after-divorce.html",
 		"Buying a House After Divorce",
-		"9/9/2020",
 		"Tips for Buying a House after Divorce"
 	),
 	new Article(
 		"https://www.womansdivorce.com/can-i-file-a-joint-tax-return.html",
 		"Can I File A Joint Tax Return",
-		"3/15/2007",
 		"Can I File a Joint Tax Return?"
+	),
+	new Article(
+		"https://www.womansdivorce.com/capital-gains-tax-in-divorce-settlements.html",
+		"Capital Gains Tax in Divorce Settlements",
+		"Why You Should Consider Capital Gains Taxes in Your Divorce Settlement"
+	),
+	new Article(
+		"https://www.womansdivorce.com/causes-of-marital-affairs.html",
+		"Causes of Marital Affairs",
+		"Why Did He Cheat? Understanding the Causes of Marital Affairs"
 	),
 	new Article(
 		"https://www.womansdivorce.com/cell-phone-voice-messages.html",
 		"Cell Phone Voice Messages",
-		"12/19/2007",
 		"Cell Phone Voice Messages and Custody Disputes"
+	),
+	new Article(
+		"https://www.womansdivorce.com/change-after-divorce.html",
+		"Change After Divorce",
+		"Tips for Helping Your Kids Deal with Change after Divorce"
+	),
+	new Article(
+		"https://www.womansdivorce.com/changing-visitation.html",
+		"Changing Visitation",
+		"Changing Visitation - Common Reasons Parents Want to Limit Visitation"
 	),
 	new Article(
 		"https://www.womansdivorce.com/cheap-divorce.html",
 		"Cheap Divorce",
-		"10/30/2014",
 		"When a Cheap Divorce Backfires"
 	),
 	new Article(
 		"https://www.womansdivorce.com/cheating-husband.html",
 		"Cheating Husband",
-		"12/11/2007",
 		"Infidelity: What to Look For, What to Do"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-behavior-during-divorce.html",
+		"Child Behavior During Divorce",
+		"How Normal Childhood Behavior during Divorce can be Misinterpreted"
 	),
 	new Article(
 		"https://www.womansdivorce.com/childcare-after-divorce.html",
 		"Childcare after Divorce",
-		"9/3/2022",
 		"How are Child Care Expenses Divided After Divorce?"
 	),
 	new Article(
 		"https://www.womansdivorce.com/child-chooses-to-change-custody.html",
 		"Child Chooses To Change Custody",
-		"6/2/2006",
 		"How do you cope when your child chooses to change custody? "
 	),
+	new Article(
+		"https://www.womansdivorce.com/child-chooses-to-live.html",
+		"Child Chooses To Live",
+		"How to Handle When Your Child Chooses to Live With the Other Parent"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-custody-agreement.html",
+		"Child Custody Agreement",
+		"Creating a Child Custody Agreement"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-custody-and-incarceration.html",
+		"Child Custody and Incarceration",
+		"Child Custody and Incarceration: How Being in Jail or Prison affects Parental Rights"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-custody-and-visitation.html",
+		"Child Custody and Visitation",
+		"Child Custody and Visitation Basics"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-custody-battles.html",
+		"Child Custody Battles",
+		"Child Custody Battles – Getting Prepared to Prove Why You Should Get Custody"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-custody-calendar.html",
+		"Child Custody Calendar",
+		"How an Online Child Custody Calendar can Make Co-Parenting Easier"
+	),
+	new Article(
+		"https://www.womansdivorce.com/child-custody-changes.html",
+		"Child Custody Changes",
+		"FAQs on Child Custody Changes – Which Situations Warrant a Change in Custody"
+	),
+	new Article("", "", ""),
 ];
