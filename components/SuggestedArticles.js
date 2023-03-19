be5881f50af29be6d1612f11ef5fd5e3bedbdf52
Justin Scorzafava <jkscorzafava@gmail.com>
2023-03-19 13:09:08 -0600

    worked on styling outerContainer for suggestedArticlesModal

--- components/SuggestedArticles.js
+++ components/SuggestedArticles.js
@@ -1,4 +1,5 @@
 import { View, Text, StyleSheet, Animated } from "react-native";
+import Colors from "../constants/colors";
 
 const SuggestedArticles = () => {
 	return (
@@ -13,7 +14,15 @@ export default SuggestedArticles;
 const styles = StyleSheet.create({
 	outerContainer: {
 		position: "absolute",
-		top: "60%",
+		top: "25%",
+		bottom: "0%",
 		zIndex: 50,
+		backgroundColor: "white",
+		width: "100%",
+		minHeight: "160%",
+		borderWidth: 4,
+		borderColor: Colors.accent500,
+		borderTopRightRadius: 4,
+		borderTopLeftRadius: 4,
 	},
 });
