d338273510ff90c2442916e4c50bdf73ab940542
Justin Scorzafava <jkscorzafava@gmail.com>
2023-03-19 11:38:55 -0600

    created component for ArticlePageButton and added some import statements

---
+++ components/ArticlePageButton.js
@@ -0,0 +1,8 @@
+import { Pressable, Text, StyleSheet, Animated, Linking } from "react-native";
+import Colors from "../constants/colors";
+
+const ArticlePageButton = () => {
+	return <div>ArticlePageButton</div>;
+};
+
+export default ArticlePageButton;
