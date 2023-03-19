3d0556cc408a011bb5e45a8a3440de3fe52b2b9d
Justin Scorzafava <jkscorzafava@gmail.com>
2023-03-19 12:13:42 -0600

    added styles to start with and applied styles to JSX

--- components/ArticlePageButton.js
+++ components/ArticlePageButton.js
@@ -3,9 +3,16 @@ import Colors from "../constants/colors";
 
 const ArticlePageButton = () => {
 	return (
-		<Animated.View>
-			<Pressable>
-				<Text>View Suggested Articles</Text>
+		<Animated.View style={styles.buttonOuterContainer}>
+			<Pressable
+				style={({ pressed }) =>
+					pressed
+						? [styles.buttonInnerContainer, styles.pressed]
+						: styles.buttonInnerContainer
+				}
+				android_ripple={{ color: Colors.primary700 }}
+			>
+				<Text style={styles.buttonText}>Browse a Few Suggested Articles</Text>
 			</Pressable>
 		</Animated.View>
 	);
@@ -13,4 +20,23 @@ const ArticlePageButton = () => {
 
 export default ArticlePageButton;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	buttonOuterContainer: {
+		borderRadius: 28,
+		marginHorizontal: 36,
+		overflow: "hidden",
+	},
+	buttonInnerContainer: {
+		backgroundColor: Colors.accent600,
+		paddingVertical: 8,
+		paddingHorizontal: 16,
+		elevation: 2,
+	},
+	buttonText: {
+		color: Colors.primary800,
+		textAlign: "center",
+	},
+	pressed: {
+		opacity: 0.75,
+	},
+});
