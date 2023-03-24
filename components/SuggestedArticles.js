00da3bd46a8bb7680311aa429c4eb312dbbd421b
Justin Scorzafava <jkscorzafava@gmail.com>
2023-03-23 18:49:08 -0600

    edited display text

--- components/SuggestedArticles.js
+++ components/SuggestedArticles.js
@@ -1,12 +1,39 @@
+import { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Animated } from "react-native";
 import { ARTICLES } from "../data/ArticleData";
 import Colors from "../constants/colors";
 
 const SuggestedArticles = () => {
-	const randomNumber = Math.floor(Math.random() * (454 - 1) + 1);
+	const randomNumberOne = Math.floor(Math.random() * (454 - 1) + 1);
+	const randomNumberTwo = getRandomNumber();
+	const randomNumberThree = getRandomNumber();
+	const randomNumberFour = getRandomNumber();
+	const randomNumberFive = getRandomNumber();
+	const randomNumberSix = getRandomNumber();
+
+	const [suggestedArticleOne, setSuggestedArticleOne] = useState();
+	const [suggestedArticleTwo, setSuggestedArticleTwo] = useState();
+	const [suggestedArticleThree, setSuggestedArticleThree] = useState();
+	const [suggestedArticleFour, setSuggestedArticleFour] = useState();
+	const [suggestedArticleFive, setSuggestedArticleFive] = useState();
+	const [suggestedArticleSix, setSuggestedArticleSix] = useState();
+
+	function getRandomNumber() {
+		Math.floor(Math.random() * (454 - 1) + 1);
+	}
+
+	useEffect(() => {
+		setSuggestedArticleOne(ARTICLES[randomNumberOne]);
+		setSuggestedArticleTwo(ARTICLES[randomNumberTwo]);
+		setSuggestedArticleThree(ARTICLES[randomNumberThree]);
+		setSuggestedArticleFour(ARTICLES[randomNumberFour]);
+		setSuggestedArticleFive(ARTICLES[randomNumberFive]);
+		setSuggestedArticleSix(ARTICLES[randomNumberSix]);
+	}, []);
+
 	return (
 		<View style={styles.outerContainer}>
-			<Text>SuggestedArticles</Text>
+			<Text>{suggestedArticleOne.linkText}</Text>
 		</View>
 	);
 };
