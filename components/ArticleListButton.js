98d2edc787100df019819a1156226f2e120dd3b5
Justin Scorzafava <jkscorzafava@gmail.com>
2023-03-26 13:19:20 -0600

    added imports for new component and added Stylesheet at bottom

--- components/ArticleListButton.js
+++ components/ArticleListButton.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { Pressable, Text, StyleSheet, Animated, Linking } from "react-native";
+import Colors from "../constants/colors";
 
 const ArticleListButton = () => {
 	return <div>ArticleListButton</div>;
 };
 
 export default ArticleListButton;
+
+const styles = StyleSheet.create({});
