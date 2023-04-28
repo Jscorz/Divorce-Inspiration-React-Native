ca3eff08f450784e611b164c9bb42569e4dcd98b
Justin Scorzafava <jkscorzafava@gmail.com>
2023-04-28 17:02:46 -0600

    updated look of primary button to match article button by increasing fontsize, horiz margin, vert padding, and swithcing styling of some of those styles from outerContainer to innerContainer

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,7 +1,9 @@
-import { View, Text, Pressable, StyleSheet } from "react-native";
+import { View, Text, Pressable, StyleSheet, useWindowDimensions } from "react-native";
 import Colors from "../constants/colors";
 
 function PrimaryButton({ children, onPress, numberOfQuotesViewed }) {
+	const { width, height } = useWindowDimensions();
+
 	return (
 		<View
 			style={
@@ -10,17 +12,32 @@ function PrimaryButton({ children, onPress, numberOfQuotesViewed }) {
 					: styles.buttonOuterContainer
 			}
 		>
-			<Pressable
-				style={({ pressed }) =>
-					pressed
-						? [styles.buttonInnerContainer, styles.pressed]
-						: styles.buttonInnerContainer
-				}
-				onPress={onPress}
-				android_ripple={{ color: Colors.primary700 }}
-			>
-				<Text style={styles.buttonText}>{children}</Text>
-			</Pressable>
+			{height < 850 && (
+				<Pressable
+					style={({ pressed }) =>
+						pressed
+							? [styles.buttonInnerContainer, styles.pressed]
+							: styles.buttonInnerContainer
+					}
+					onPress={onPress}
+					android_ripple={{ color: Colors.primary700 }}
+				>
+					<Text style={styles.buttonText}>{children}</Text>
+				</Pressable>
+			)}
+			{height > 850 && (
+				<Pressable
+					style={({ pressed }) =>
+						pressed
+							? [styles.buttonInnerContainerBig, styles.pressed]
+							: styles.buttonInnerContainerBig
+					}
+					onPress={onPress}
+					android_ripple={{ color: Colors.primary700 }}
+				>
+					<Text style={styles.buttonTextBig}>{children}</Text>
+				</Pressable>
+			)}
 		</View>
 	);
 }
@@ -29,19 +46,32 @@ export default PrimaryButton;
 
 const styles = StyleSheet.create({
 	buttonOuterContainer: {
-		borderRadius: 28,
-		marginHorizontal: 36,
 		overflow: "hidden",
 	},
 	buttonInnerContainer: {
 		backgroundColor: Colors.accent500,
 		paddingVertical: 12,
 		paddingHorizontal: 16,
+		marginHorizontal: 36,
+		borderRadius: 28,
+		elevation: 2,
+	},
+	buttonInnerContainerBig: {
+		backgroundColor: Colors.accent500,
+		paddingVertical: 16,
+		paddingHorizontal: 16,
+		marginHorizontal: 140,
+		borderRadius: 28,
 		elevation: 2,
 	},
 	buttonText: {
 		color: Colors.primary800,
 		textAlign: "center",
+	},
+	buttonTextBig: {
+		color: Colors.primary800,
+		textAlign: "center",
+		fontSize: 16,
 	},
 	pressed: {
 		opacity: 0.75,
