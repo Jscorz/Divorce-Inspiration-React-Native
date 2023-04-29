303c24aea4f95894a2f35769e4312e94f7c4a2dd
Justin Scorzafava <jkscorzafava@gmail.com>
2023-04-29 14:14:07 -0600

    updated sizing of animatedSquare based on height > 1000 and conditionally styled it, had to adjust input/output range on border radius change to keep it round

--- components/AnimatedSquare.js
+++ components/AnimatedSquare.js
@@ -1,8 +1,9 @@
 import { useRef, useEffect } from "react";
-import { Animated, Text, View, StyleSheet } from "react-native";
+import { Animated, Text, View, StyleSheet, useWindowDimensions } from "react-native";
 import Colors from "../constants/colors";
 
 function AnimatedSquare() {
+	const { height, width } = useWindowDimensions();
 	const SIZE = 100;
 	const progress = useRef(new Animated.Value(0)).current;
 	const scale = useRef(new Animated.Value(1)).current;
@@ -24,25 +25,47 @@ function AnimatedSquare() {
 
 	return (
 		<Animated.View
-			style={[
-				styles.square,
-				{
-					borderRadius: progress.interpolate({
-						inputRange: [0.5, 1],
-						outputRange: [SIZE / 4, SIZE / 2],
-					}),
-					opacity: progress,
-					transform: [
-						{ scale },
-						{
-							rotate: progress.interpolate({
-								inputRange: [0.5, 1],
-								outputRange: ["180deg", `360deg`],
-							}),
-						},
-					],
-				},
-			]}
+			style={
+				height > 1000
+					? [
+							styles.squareBig,
+							{
+								borderRadius: progress.interpolate({
+									inputRange: [0.5, 0.7],
+									outputRange: [SIZE / 4, SIZE / 2],
+								}),
+								opacity: progress,
+								transform: [
+									{ scale },
+									{
+										rotate: progress.interpolate({
+											inputRange: [0.5, 1],
+											outputRange: ["180deg", `360deg`],
+										}),
+									},
+								],
+							},
+					  ]
+					: [
+							styles.square,
+							{
+								borderRadius: progress.interpolate({
+									inputRange: [0.5, 1],
+									outputRange: [SIZE / 4, SIZE / 2],
+								}),
+								opacity: progress,
+								transform: [
+									{ scale },
+									{
+										rotate: progress.interpolate({
+											inputRange: [0.5, 1],
+											outputRange: ["180deg", `360deg`],
+										}),
+									},
+								],
+							},
+					  ]
+			}
 		></Animated.View>
 	);
 }
@@ -55,5 +78,11 @@ const styles = StyleSheet.create({
 		height: 100,
 		backgroundColor: Colors.AnimatedSquareLowOpacity,
 		marginTop: 120,
+	},
+	squareBig: {
+		width: 150,
+		height: 150,
+		backgroundColor: Colors.AnimatedSquareLowOpacity,
+		marginTop: 170,
 	},
 });
