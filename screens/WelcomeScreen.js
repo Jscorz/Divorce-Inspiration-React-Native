bd9654595855fdb9fdb6c1deeaa763f3f3390888
Justin Scorzafava <jkscorzafava@gmail.com>
2023-04-29 13:58:44 -0600

    added conditional styling for header and subHeader text with extra classes for based on screen size

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,14 +1,22 @@
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, useWindowDimensions } from "react-native";
 import AnimatedSquare from "../components/AnimatedSquare";
 import Colors from "../constants/colors";
 
 function WelcomeScreen() {
+	const { height, width } = useWindowDimensions();
+
 	return (
 		<View style={styles.rootContainer}>
-			<Text style={styles.welcomeText}>Welcome back!</Text>
+			<Text style={height > 1000 ? styles.welcomeTextBig : styles.welcomeText}>
+				Welcome back!
+			</Text>
 			<View style={styles.subheaderTextContainer}>
-				<Text style={styles.subheaderText}>Get Ready For</Text>
-				<Text style={styles.subheaderText}>Your Daily Inspiration</Text>
+				<Text style={height > 1000 ? styles.subheaderTextBig : styles.subheaderText}>
+					Get Ready For
+				</Text>
+				<Text style={height > 1000 ? styles.subheaderTextBig : styles.subheaderText}>
+					Your Daily Inspiration
+				</Text>
 			</View>
 			<AnimatedSquare />
 		</View>
@@ -29,8 +37,16 @@ const styles = StyleSheet.create({
 		fontSize: 48,
 		color: Colors.accent500,
 	},
+	welcomeTextBig: {
+		fontSize: 64,
+		color: Colors.accent500,
+	},
 	subheaderText: {
 		fontSize: 24,
+		color: "white",
+	},
+	subheaderTextBig: {
+		fontSize: 36,
 		color: "white",
 	},
 	subheaderTextContainer: {
